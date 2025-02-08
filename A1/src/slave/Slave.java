ca0e68f72997211244a32f3d7056840ee31f6053
Daniel Ramsgard <dramsgard@gmail.com>
2025-02-08 11:32:28 -0500

    tostring slave

---
+++ A1/src/slave/Slave.java
@@ -0,0 +1,16 @@
+package slave;
+
+import gradingTools.comp533s19.assignment0.AMapReduceTracer;
+
+public class Slave extends AMapReduceTracer implements Runnable {
+	public static String SLAVE = "Slave";
+	
+	public void run() {
+		
+	}
+	
+	public String toString() {
+		return SLAVE;
+	}
+
+}
