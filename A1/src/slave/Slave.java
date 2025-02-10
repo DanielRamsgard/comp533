d641ac6cae6486f006b4186ed275b08b0fcf4087
Daniel Ramsgard <dramsgard@gmail.com>
2025-02-09 22:10:49 -0500

    multiple input strings

--- A1/src/slave/Slave.java
+++ A1/src/slave/Slave.java
@@ -16,6 +16,7 @@ public class Slave extends AMapReduceTracer implements Runnable {
 	private final int identifier;
 	private final Model model;
 	private final List<KeyValue<String, Integer>> inputList;
+	private boolean exitEarly = false;
 	
 	public Slave(int identifier, Model model) {
 		this.identifier = identifier;
@@ -54,8 +55,8 @@ public class Slave extends AMapReduceTracer implements Runnable {
 			super.traceWait();
 			this.wait();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			super.traceQuit();
+			exitEarly = true;
 		}
 	}
 	
@@ -67,26 +68,33 @@ public class Slave extends AMapReduceTracer implements Runnable {
 	public void run() {
 		// get items
 		while (true) {
-			KeyValue<String, Integer> currentKeyValue = null;
-			
-			try {
-				currentKeyValue = model.getBlockingQueue().take();
-				super.traceDequeue(currentKeyValue);
-			} catch (InterruptedException e) {
-				super.traceQuit();
-				e.printStackTrace();
+			if (exitEarly) {
 				break;
 			}
 			
-			if (currentKeyValue != null && currentKeyValue.getKey() != null && currentKeyValue.getValue() != null) {
-				inputList.add(currentKeyValue);
-			} else if (currentKeyValue != null) {
-				break;
+			while (true) {
+				KeyValue<String, Integer> currentKeyValue = null;
+				
+				try {
+					currentKeyValue = model.getBlockingQueue().take();
+					super.traceDequeue(currentKeyValue);
+				} catch (InterruptedException e) {
+					super.traceQuit();
+					exitEarly = true;
+					
+					break;
+				}
+				
+				if (currentKeyValue != null && currentKeyValue.getKey() != null && currentKeyValue.getValue() != null) {
+					inputList.add(currentKeyValue);
+				} else if (currentKeyValue != null) {
+					break;
+				}
 			}
-		}
-		
-		// produce partially reduced map
-		produceMap();
+			
+			// produce partially reduced map
+			produceMap();
+		}		
 	}
 	
 	public String toString() {
