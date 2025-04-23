0497162b3daccabf125ac877aa443d53994e9b6a
Daniel Ramsgard <dramsgard@gmail.com>
2025-04-23 19:28:04 -0400

    start with rmi

--- A4/src/client/CoupledClientSimulation.java
+++ A4/src/client/CoupledClientSimulation.java
@@ -98,7 +98,7 @@ public class CoupledClientSimulation extends AnAbstractSimulationParametersBean 
 	public CoupledClientSimulation() {
 		setTracing();
 		this.messagesQueue = new ArrayBlockingQueue<>(100000);
-		setIPCMechanism(IPCMechanism.NIO);				
+		setIPCMechanism(IPCMechanism.RMI);				
 		setBroadcastMetaState(true);
 		this.nioManager = NIOManagerFactory.getSingleton();
 		this.configurer = new ClientConfigurer(messagesQueue, nioManager);
